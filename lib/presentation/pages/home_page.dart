754cc048b495ccaa2d14286d2628469812cf83b3
Michel Dufour <github@michel-dufour.fr>
2023-10-05 20:39:28 +0200

    Met à jour la HomePage

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gps_tracker/bloc/geolocation_cubit.dart';
+import 'package:gps_tracker/presentation/components/geolocator_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,10 +10,12 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(title: const Text('Geolocator'),
-    ),
-    body: const Center(
-          child: Text('Hello Mitch'),
+        appBar: AppBar(
+          title: const Text('Geolocator'),
         ),
-  );
+        body: BlocProvider<GeolocationCubit>(
+          create: (_) => GeolocationCubit()..streamGeolocation(),
+          child: const GeolocatorWidget(),
+        ),
+      );
 }
